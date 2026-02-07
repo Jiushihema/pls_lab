45f373272aace145646b9d7e32d0a6f11c6ddcfb
hema <heguanglin@gmail.com>
2026-02-07 10:35:47 +0800

    link solve

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-7.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-f9684023 .uagb-swiper",
+		{"autoplay":{"delay":2016,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":1290,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-f9684023 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-f9684023 .swiper-button-next","prevEl":".uagb-block-f9684023 .swiper-button-prev"}}	);
+});
+
+ });
